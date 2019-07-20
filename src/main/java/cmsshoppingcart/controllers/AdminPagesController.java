542e9e1ee7ec61fab58e26a60dbe4afd37ee717f
Brett Hartman <brett.hartman@mac.com>
2019-07-20 07:12:31 -0700

    Display pages on admin dashboard

--- src/main/java/cmsshoppingcart/controllers/AdminPagesController.java
+++ src/main/java/cmsshoppingcart/controllers/AdminPagesController.java
@@ -1,14 +1,27 @@
 package cmsshoppingcart.controllers;
 
+import cmsshoppingcart.models.PageRepository;
+import cmsshoppingcart.models.data.Page;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin/pages")
 public class AdminPagesController {
+  private final PageRepository pageRepository;
+
+  public AdminPagesController(PageRepository pageRepository) {
+    this.pageRepository = pageRepository;
+  }
+
   @GetMapping
-  public String index() {
+  public String index(Model model) {
+    List<Page> pages = pageRepository.findAll();
+    model.addAttribute("pages", pages);
+
     return "admin/pages/index";
   }
 }
