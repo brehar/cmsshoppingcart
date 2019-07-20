2253792e67630a8eeca16d222c8442a56156549e
Brett Hartman <brett.hartman@mac.com>
2019-07-20 08:05:46 -0700

    Implement ability to add a Page

--- src/main/java/cmsshoppingcart/controllers/AdminPagesController.java
+++ src/main/java/cmsshoppingcart/controllers/AdminPagesController.java
@@ -3,11 +3,15 @@ package cmsshoppingcart.controllers;
 import cmsshoppingcart.models.PageRepository;
 import cmsshoppingcart.models.data.Page;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin/pages")
@@ -29,5 +33,39 @@ public class AdminPagesController {
   @GetMapping("/add")
   public String add(@ModelAttribute Page page) {
     return "admin/pages/add";
+  }
+
+  @PostMapping("/add")
+  public String add(
+      @Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    if (bindingResult.hasErrors()) {
+      return "admin/pages/add";
+    }
+
+    redirectAttributes.addFlashAttribute("message", "Page added successfully.");
+    redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+    String slug =
+        page.getSlug().equals("")
+            ? page.getTitle().toLowerCase().replace(" ", "-")
+            : page.getSlug().toLowerCase().replace(" ", "-");
+
+    Page slugExists = pageRepository.findBySlug(slug);
+
+    if (slugExists != null) {
+      redirectAttributes.addFlashAttribute(
+          "message", "The slug you selected already exists. Please choose another.");
+      redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+      redirectAttributes.addFlashAttribute("page", page);
+
+      return "redirect:/admin/pages/add";
+    }
+
+    page.setSlug(slug);
+    page.setSorting(100);
+
+    pageRepository.save(page);
+
+    return "redirect:/admin/pages";
   }
 }
--- src/main/java/cmsshoppingcart/models/PageRepository.java
+++ src/main/java/cmsshoppingcart/models/PageRepository.java
@@ -3,4 +3,6 @@ package cmsshoppingcart.models;
 import cmsshoppingcart.models.data.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PageRepository extends JpaRepository<Page, Integer> {}
+public interface PageRepository extends JpaRepository<Page, Integer> {
+  Page findBySlug(String slug);
+}
