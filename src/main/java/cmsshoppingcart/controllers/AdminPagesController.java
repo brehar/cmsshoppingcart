d414f615e5a0b610902cb85a17c78f1ba1cad508
Brett Hartman <brett.hartman@mac.com>
2019-07-20 08:17:45 -0700

    Create edit Page form

--- src/main/java/cmsshoppingcart/controllers/AdminPagesController.java
+++ src/main/java/cmsshoppingcart/controllers/AdminPagesController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -67,5 +68,13 @@ public class AdminPagesController {
     pageRepository.save(page);
 
     return "redirect:/admin/pages";
+  }
+
+  @GetMapping("/edit/{id}")
+  public String edit(@PathVariable int id, Model model) {
+    Page page = pageRepository.getOne(id);
+    model.addAttribute("page", page);
+
+    return "admin/pages/edit";
   }
 }
